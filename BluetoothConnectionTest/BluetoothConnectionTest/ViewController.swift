43f01437225b3d109b0f3d4e948d230bb6993edf
植原　駿 <ueponmasuta@gmail.com>
2017-06-17 17:05:29 +0900

    プロジェクト作成

---
+++ BluetoothConnectionTest/BluetoothConnectionTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BluetoothConnectionTest
+//
+//  Created by 植原　駿 on 2017/06/17.
+//  Copyright © 2017年 Shun Uehara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
