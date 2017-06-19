0b8ea813b140a17ae061390db5f381e45e1af22c
植原　駿 <ueponmasuta@gmail.com>
2017-06-20 00:26:03 +0900

    tableViewのサンプル作成

--- BluetoothConnectionTest/BluetoothConnectionTest/ViewController.swift
+++ BluetoothConnectionTest/BluetoothConnectionTest/ViewController.swift
@@ -8,16 +8,71 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    /// test用配列
+    var arr = ["test1", "test2", "test3"]
+    
+    /// Bluetooth接続先リスト
+    var connectionList: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Status Barの高さを取得する.
+        let barHeight: CGFloat = UIApplication.shared.statusBarFrame.size.height
+        
+        // Viewの高さと幅を取得する.
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        
+        // TableViewの生成(Status barの高さをずらして表示).
+        connectionList = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight))
+        
+        // Cell名の登録をおこなう.
+        connectionList.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        
+        // DataSourceを自身に設定する.
+        connectionList.dataSource = self
+        
+        // Delegateを自身に設定する.
+        connectionList.delegate = self
+        
+        // Viewに追加する.
+        self.view.addSubview(connectionList)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    /*
+     Cellが選択された際に呼び出される
+     */
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Num: \(indexPath.row)")
+        print("Value: \(arr[indexPath.row])")
+    }
+    
+    /*
+     Cellの総数を返す.
+     */
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count
+    }
+    
+    /*
+     Cellに値を設定する
+     */
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 再利用するCellを取得する.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
+        
+        // Cellに値を設定する.
+        cell.textLabel!.text = "\(arr[indexPath.row])"
+        
+        return cell
     }
 
 
